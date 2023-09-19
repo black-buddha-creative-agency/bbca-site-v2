c4c5472065fa9cc9329eb24d9d6bf319da830690
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2023-09-19 16:21:01 -0500

    chore: update gitignore

---
+++ constants.ts
@@ -0,0 +1,7 @@
+import { Metadata } from "next";
+
+export const studioMeta: Metadata = {
+	title: "Black Buddha Creative Agency - Content Studio",
+	description:
+		"The content studio for Black Buddha Creative Agency's official website.",
+};
